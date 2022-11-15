43f4abdf856d310915a54d4ccfb3350d8307f753
wzc520pyfm <1528857653@qq.com>
2022-11-15 19:24:16 +0800

    feat: 增加微信回调接口, 增加轮询验证登录接口

--- router/wxLogin.js
+++ router/wxLogin.js
@@ -2,10 +2,14 @@ const express = require('express')
 const router = express.Router()
 const WxLoginController = require('../controller/WxLoginController.js')
 
-// 注册接口
+// 微信验证接入接口
 router.get('/callback', WxLoginController.wechat_insert)
 // 获取二维码接口
 router.get('/login', WxLoginController.login)
+// (用户扫码后)微信回调发送用户信息接口
+router.post('/callback', WxLoginController.wechat_message)
+// 轮询用户是否扫码
+router.get('/check_scan', WxLoginController.check_scan)
 
 
 module.exports = router
--- service/WxLoginService.js
+++ service/WxLoginService.js
@@ -2,6 +2,10 @@ const SecretTool = require('../utils/SecretTool')
 const { getOR } = require('../config/wechatLogin')
 const redisConfig = require('../config/redisConfig')
 const BackCode = require('../utils/BackCode')
+const CodeEnum = require('../utils/CodeEnum')
+const WxDataTool = require('../utils/WxDataTool')
+const DB = require('../config/sequelize')
+const RandomTool = require('../utils/RandomTool')
 const { WX_TOKEN } = process.env
 
 const WxLoginService = {
@@ -21,6 +25,69 @@ const WxLoginService = {
     let key = `wechat:ticket:${ticket}`
     redisConfig.set(key, JSON.stringify({ isScan: 'no' }, 120))
     return BackCode.buildSuccessAndData({ data: { qrcodeUrl, ticket } })
+  },
+  wechat_message: async (req) => {
+    // 处理微信发送的数据
+    let xmlData = await WxDataTool.getXMLStr(req)
+    let objectData = await WxDataTool.getObject(xmlData)
+    let lastData = WxDataTool.getLastData(objectData.xml)
+
+    // 根据openid判断是否注册过
+    let openidRes = await DB.Account.findAll({ where: { openid: lastData.FromUserName }, /** 返回原始对象 */raw: true })
+    // 随机生成用户的头像和昵称
+    let head_img = RandomTool.randomAvatar()
+    let username = RandomTool.randomName()
+    let user = null
+    // 未注册则插入数据
+    if (openidRes.length === 0) {
+      let resData = await DB.Account.create({ head_img, username, openid: lastData.FromUserName })
+      user = { head_img, username, id: resData.toJSON().id } // toJSON也可以达到raw: true的效果
+    } else {
+      // 注册过
+      user = { head_img: openidRes.head_img, username: openidRes.username, id: openidRes.id }
+    }
+
+    // 生成token
+    let token = SecretTool.jwtSign(user, '168h')
+
+    // 更新redis状态
+    let key = `wechat:ticket:${lastData.Ticket}`
+    const existsKey = await redisConfig.exists(key)
+    if (existsKey) {
+      redisConfig.set(key, JSON.stringify({ isScan: 'yes', token }), 120)
+    }
+
+    // 返回微信服务器的内容
+    let content = ''
+    if (lastData.MsgType === 'event') {
+      if (lastData.Event === 'SCAN') { // 扫码, 不是第一次登录
+        content = '欢迎回来!'
+      } else if (lastData.Event === 'subscribe') { // 订阅/关注, 第一次扫码登录
+        content = '感谢关注!'
+      }
+
+      // 构建xml数据
+      let msgStrXml = `<xml>
+        <ToUserName><![CDATA[${lastData.FromUserName}]]></ToUserName>
+        <FromUserName><![CDATA[${lastData.ToUserName}]]></FromUserName>
+        <CreateTime>${Date.now()}</CreateTime>
+        <MsgType><![CDATA[text]]></MsgType>
+        <Content><![CDATA[${content}]]></Content>
+      </xml>`
+
+      return msgStrXml
+    }
+  },
+  check_scan: async (req) => {
+    let { ticket } = req.query
+    let key = `wechat:ticket:${ticket}`
+    let redisData = JSON.parse(await redisConfig.get(key))
+    if (redisData && redisData.isScan === 'yes') {
+      let { token } = redisData
+      return BackCode.buildSuccessAndData({ data: `Bearer ${token}` })
+    } else {
+      return BackCode.buildResult(CodeEnum.WECHAT_WAIT_SCAN)
+    }
   }
 }
 
